bfaff7a21517230f3f0775ed3d7b2b5e218d39aa
datphamcode295 <vandatpro2000@gmail.com>
2022-04-20 15:44:52 +0700

    initial code

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const employeeRouter = require('./routes/EmployeeRoutes.js');
+
+const app = express();
+app.use(express.json()); // Make sure it comes back as json
+
+//TODO - Replace you Connection String here
+mongoose.connect('mongodb+srv://datbk:123456789dat@cluster0.punaw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(success => {
+  console.log('Success Mongodb connection')
+}).catch(err => {
+  console.log('Error Mongodb connection')
+});
+
+app.use(employeeRouter);
+
+app.listen(8081, () => { console.log('Server is running...') });
